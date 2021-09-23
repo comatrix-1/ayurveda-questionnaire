7a147ca1c14b81ec387c0b388ba9767cbbf1958b
comatrix-1 <84917515+comatrix-1@users.noreply.github.com>
2021-09-23 09:29:13 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const table = `
 ,VATA,Prakruti,Vikruti,PITTA,Prakruti,Vikruti,KAPHA,Prakruti,Vikruti
 Age,55+,,,17-55,,,0-16,,
 `
+console.log("hello world");
 
-const body = document.querySelector("body");
+// const body = document.querySelector("body");
 
-body.append(table);
+// body.append(table);
